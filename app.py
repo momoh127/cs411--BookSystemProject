f305712befdbe33b43876aa6250f8a6b6f182c89
malsaif <mozr2727@gmail.com>
2024-12-10 16:21:57 -0500

    finished writing the dockerfile and tested it. Fixed several bugs and wrote requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, g
 from routes.auth_routes import auth_blueprint
 from routes.book_routes import book_blueprint
+from databases.database_setup import create_database
 from config import SECRET_KEY
+import os
+import sqlite3
 
 app = Flask(__name__)
 
 # Set the secret key for session management
 app.config["SECRET_KEY"] = SECRET_KEY
+
+# Path to the SQLite database file
+DATABASE_PATH = os.path.join("databases", "books.db")
+create_database()
+
+def get_db():
+    """
+    Opens a new database connection if there is none yet for the current application context.
+    """
+    if "db" not in g:
+        g.db = sqlite3.connect(DATABASE_PATH)
+        g.db.row_factory = sqlite3.Row  # Optional: To return rows as dictionaries
+    return g.db
+
+@app.teardown_appcontext
+def close_db(exception):
+    """
+    Closes the database again at the end of the request or application context.
+    """
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+
 
 # for authentication and book-related routes
 app.register_blueprint(auth_blueprint, url_prefix="/auth")
